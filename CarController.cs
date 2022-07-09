034940f729cf5bba417488212c7b6a4bbf02c8ec
Yuki-fujiif <84620909+Yuki-fujiif@users.noreply.github.com>
2022-07-09 21:53:35 +0900

    Add files via upload
    スワイプの長さで移動距離を決めるスクリプト

---
+++ CarController.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+    float speed = 0;
+    Vector2 startPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Application.targetFrameRate = 60;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //スワイプの長さを求める
+        if (Input.GetMouseButtonDown(0))
+        {
+            //マウスをクリックした座標
+            this.startPos = Input.mousePosition;
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            //マウスを離した座標
+            Vector2 endPos = Input.mousePosition;
+            float swipeLength = endPos.x - this.startPos.x;
+
+            //スワイプの長さを初速度に変換
+            this.speed = swipeLength / 500.0f;
+        }
+        if (Input.GetMouseButtonDown(0))
+        {
+            this.speed = 0.2f; //初速を設定
+        }
+
+        transform.Translate(this.speed, 0, 0); //移動
+        this.speed *= 0.98f; //減速
+    }
+}
